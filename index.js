586657d1260485843ef85546c318c414729da3a7
Niels Koster <niels.koster@portavita.nl>
2021-03-18 16:09:06 +0100

    Consume a topic from an offset, prototype

--- index.js
+++ index.js
@@ -5,6 +5,55 @@
   const app = express()
   const cors = require('cors')
 
+  const { Kafka } = require('kafkajs')
+
+  const kafka = new Kafka({
+    clientId: 'offsetter',
+    brokers: ['pvdevkafka01:9092']
+  })
+
+  const consumer = kafka.consumer({ groupId: 'test-group' })
+
+  await consumer.connect()
+  await consumer.subscribe({ topic: 'fhir4.capybara.firefly.medicom.observation', offset: '20139091' })
+
+  await consumer.run({
+    autoCommit: false,
+    eachBatchAutoResolve: true,
+    eachBatch: async ({
+        batch,
+        resolveOffset,
+        heartbeat,
+        commitOffsetsIfNecessary,
+        uncommittedOffsets,
+        isRunning,
+        isStale,
+    }) => {
+        if (isStale()) {
+          return
+        }
+        for (let message of batch.messages) {
+            console.log({
+                topic: batch.topic,
+                partition: batch.partition,
+                highWatermark: batch.highWatermark,
+                message: {
+                    offset: message.offset,
+                    key: message.key.toString(),
+                    value: message.value.toString(),
+                    headers: message.headers,
+                }
+            })
+            consumer.pause()
+            break;
+            // resolveOffset(message.offset)
+            // await heartbeat()
+        }
+    },
+  })
+  
+  await consumer.seek({ topic: 'fhir4.capybara.firefly.medicom.observation', partition: 0, offset: 20139091 })
+  
   const consoleLog = (...args) => {
     process.stdout.write(`${new Date().toUTCString()}:`)
     args.forEach(arg => process.stdout.write(` ${arg.toString()}`))
