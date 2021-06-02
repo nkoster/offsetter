d1eda2e8e60b9a0afb06c53a631efef12f73ed78
Niels Koster <niels.koster@portavita.nl>
2021-06-02 13:42:47 +0200

    ATM-2771: Changes that were made after moving to a FaaS function, and moving back to a listening API

--- index.js
+++ index.js
@@ -5,92 +5,46 @@
   const express = require('express')
   const app = express()
   const cors = require('cors')
-  const { Kafka } = require('kafkajs')
 
-  const kafka = new Kafka({
-    clientId: 'offsetter',
-    brokers: ['pvdevkafka01:9092']
-  })
+  const broker = 'pvdevkafka01'
 
-  const consumer = kafka.consumer({ groupId: 'offsetter' })
-
-  const consoleLog = (...args) => {
-    process.stdout.write(`${new Date().toUTCString()}:`)
-    args.forEach(arg => process.stdout.write(` ${arg?.toString()}`))
-    process.stdout.write('\n')
-  }
+  const { exec } = require('child_process')
+  const execute = (cmd, callback) => exec(cmd, (_, stdout) => callback(stdout))
 
   app.use(cors())
   app.use(express.json())
 
   const api = async (req, res) => {
-    try {
-      res.setHeader('Content-Type', 'application/json')
-      console.log(req.method)
-      let topic, offset
-      if (req.method === 'POST') {
-        topic = req.body.topic
-        offset = req.body.offset
-      } 
-      if (req.method === 'GET') {
-        topic = req.params.topic
-        offset = req.params.offset
-      }
-      DEBUG && consoleLog('GET KAFKA', topic, offset)
-      try {
-        await consumer.disconnect()
-        await consumer.connect()
-        await consumer.subscribe({ topic, fromBeginning: true })      
-      } catch(err) {
-        consoleLog(err)
-      }
-      let kafkaMessage = {}
-      let partition = 0
-      try {
-        await consumer.run({
-          autoCommit: false,
-          eachBatchAutoResolve: true,
-          eachBatch: async ({ batch, isStale }) => {
-            if (isStale()) {
-              return
-            }
-            for (let message of batch.messages) {
-              kafkaMessage = {
-                topic: batch.topic,
-                partition: batch.partition,
-                highWatermark: batch.highWatermark,
-                message: {
-                  offset: message.offset,
-                  key: message.key.toString(),
-                  value: message.value.toString(),
-                  headers: message.headers
-                }
-              }
-              partition = batch.partition
-              DEBUG && consoleLog('OFFSET', kafkaMessage)
-              consumer.pause([{ topic: batch.topic, partitions: [batch.partition] }])
-              consumer.disconnect()
-              break
-            }
-            res.end(JSON.stringify(kafkaMessage))
-          }
-        })
-        consumer.seek({ topic, partition, offset })
-      } catch(err) {
-        consoleLog(err)
-        res.end({ 'error': err.message })
-      }
-    } catch(err) {
-      consoleLog(err)
-      res.end({ 'catched': err.message })
+    res.setHeader('Content-Type', 'application/json')
+    let topic, partition, offset
+    if (req.method === 'POST') {
+      topic = req.body.topic
+      partition = req.body.partition
+      offset = req.body.offset
+    } 
+    if (req.method === 'GET') {
+      topic = req.params.topic
+      partition = req.params.partition
+      offset = req.params.offset
     }
+    console.log(topic, partition, offset)
+    const data = await new Promise((resolve, reject) => {
+      try {
+        const cmd = `kafkacat -C -b ${broker} -t ${topic} -p ${partition} -o ${offset} -c 1 -e -q`
+        execute(cmd, result => resolve(result))
+      } catch (err) {
+        console.log(err.message)
+        reject({})
+      }
+    })
+    res.end(data)
   }
 
   app.get('/api/v1/:topic/:offset', api)
   app.post('/api/v1/kafka', api)
 
   app.listen(apiPort, _ => 
-    consoleLog('Offsetter at port', apiPort)
-  ).on('error', err => consoleLog(err.message))
+    console.log('Offsetter at port', apiPort)
+  ).on('error', err => console.log(err.message))
 
 })()
